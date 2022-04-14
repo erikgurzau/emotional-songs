f3854cd7828c7f8c385e9e9e2316894f5687e015
Erik Gurzau <94187872+erikgurzau@users.noreply.github.com>
2022-04-14 16:36:48 +0200

    Add files via upload

--- src/it/uninsubria/app/users/User.java
+++ src/it/uninsubria/app/users/User.java
@@ -2,12 +2,43 @@ package it.uninsubria.app.users;
 
 import it.uninsubria.app.users.utils.Address;
 
+/**
+ * Classe che definisce l'
+ * @author  Erik Gurzau
+ * @author  Alessia Metaj
+ * @author  Sara Biavaschi
+ * @version 1.0.0
+ * @see     it.uninsubria.app.users.Person
+ * @see     it.uninsubria.app.users.utils.Address
+ * @see     it.uninsubria.app.controllers.utils.SecurePassword
+ * @see     it.uninsubria.app.users.exceptions.UserException
+ */
 public class User extends Person {
-   private String userId;
-    private String email;
-   private String psw;
+    /**
+     * Stringa che identifica in modo univoco ogni utente
+     */
+    private final String userId;
+    /**
+     * Email dell'utente
+     */
+    private final String email;
+    /**
+     * Password del'utente
+     */
+    private final String psw;
 
+    private final String[] domains = { "@gmail.com", "@outlook.com", "@icloud.com", "@yahoo.com" };
 
+    /**
+     * Costruttore di un utente
+     * @param name Stringa che contiene il nome dell'utente
+     * @param surname Stringa che contiene il cognome dell'utente
+     * @param cf Stringa che contiene il codice fiscale dell'utente, composta da obbligatoriamente 16 caratteri
+     * @param address Indirizzo dell'utente
+     * @param userId Stringa che contiene l'ID dell'utente
+     * @param email Stringa che contiene l'email dell'utente
+     * @param psw Stringa che contiene la password
+     */
     public User(String name, String surname, String cf, Address address, String userId, String email, String psw) {
         super(name, surname, cf, address);
         this.userId = userId;
