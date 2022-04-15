da9a8231f48a2ae1c53f27960e9a14c5388ae320
Erik Gurzau <94187872+erikgurzau@users.noreply.github.com>
2022-04-15 18:38:01 +0200

    Add files via upload

---
+++ src/it/uninsubria/app/config/FileConfiguration.java
@@ -0,0 +1,8 @@
+package it.uninsubria.app.config;
+
+public interface FileConfiguration {
+    String pathFileUsers = "./data/UtentiRegistrati.txt";
+    String pathFileSongs = "./data/Canzoni.txt";
+    String pathFilePLaylists = "./data/Playlist.txt";
+    String pathFileEmotions = "./data/Emozioni.txt";
+}
