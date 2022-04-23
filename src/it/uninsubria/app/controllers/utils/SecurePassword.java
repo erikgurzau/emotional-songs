eb3911b0d03d5113897018d734037fd4b52cb5b0
sarabiava <103214669+sarabiava@users.noreply.github.com>
2022-04-23 17:01:30 +0200

    Add files via upload

--- src/it/uninsubria/app/controllers/utils/SecurePassword.java
+++ src/it/uninsubria/app/controllers/utils/SecurePassword.java
@@ -3,8 +3,26 @@ package it.uninsubria.app.controllers.utils;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Random;
 
 public class SecurePassword {
+
+    public static void main(String[] args) {  //prova del metodo genPsw()
+        System.out.println(genPsw());
+    }
+
+    public static final String CARATTERI = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&'()*+,-./:;<=>?@[]^_{|}~";
+
+    public static final int LENGTH_PSW = 16;
+
+    public static String genPsw() {         //genera una password casuale da 16 caratteri
+        Random rnd = new Random();
+        StringBuilder sb = new StringBuilder(LENGTH_PSW);
+        for(int i=0; i<LENGTH_PSW; i++) {
+            sb.append(CARATTERI.charAt(rnd.nextInt(CARATTERI.length())));
+        }
+        return sb.toString();
+    }
 
     public static String encrypt(String password){
         return sha1(password);
