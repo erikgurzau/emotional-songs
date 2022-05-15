63c8c391b2caf5761986a3ec7ad26df7dbe8d5c8
Erik Gurzau <94187872+erikgurzau@users.noreply.github.com>
2022-05-15 20:41:47 +0200

    Delete src/it/uninsubria/app directory

--- src/it/uninsubria/app/input/exceptions/InputException.java
+++
@@ -1,8 +0,0 @@
-package it.uninsubria.app.input.exceptions;
-
-public class InputException extends Exception {
-
-    public InputException(String msg){
-        super(msg);
-    }
-}
--- src/it/uninsubria/app/users/exceptions/TypeStreetException.java
+++
@@ -1,9 +0,0 @@
-package it.uninsubria.app.users.exceptions;
-
-public class TypeStreetException extends Exception {
-
-    public TypeStreetException(String msg){
-        super(msg);
-    }
-
-}
