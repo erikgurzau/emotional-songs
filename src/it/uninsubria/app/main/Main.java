d16693f596e42891bf07968240306cf9fe6685cb
sarabiava <103214669+sarabiava@users.noreply.github.com>
2022-08-11 13:02:59 +0200

    Add files via upload

--- src/it/uninsubria/app/main/Main.java
+++ src/it/uninsubria/app/main/Main.java
@@ -1,7 +1,10 @@
 package it.uninsubria.app.main;
 
+import it.uninsubria.app.emotionalsongs.Emotion;
 import it.uninsubria.app.emotionalsongs.EmotionalSongs;
 import it.uninsubria.app.input.Input;
+import it.uninsubria.app.managers.PlaylistsManager;
+import it.uninsubria.app.managers.utils.FileManager;
 import it.uninsubria.app.managers.utils.SecurePassword;
 import it.uninsubria.app.songs.Playlist;
 import it.uninsubria.app.songs.Song;
@@ -10,6 +13,8 @@ import it.uninsubria.app.users.exceptions.UserException;
 import it.uninsubria.app.users.utils.Address;
 import it.uninsubria.app.users.utils.TypeStreet;
 import it.uninsubria.app.views.Display;
+
+import java.util.Vector;
 
 public class Main {
 
@@ -102,6 +107,39 @@ public class Main {
                         Display.printAuthFailed(e.getMessage());
                     }
                     break;
+                case 3:
+
+                    if(app.isLogged()) {
+
+                        Display.printSubtitle("\nPROFILO UTENTE");
+
+                        Display.printSectionTitle("\nUser ID: ");
+                        System.out.println(app.getSessionUser().getUserId());
+
+                        Display.printSectionTitle("\nEmail: ");
+                        System.out.println(app.getSessionUser().getEmail());
+
+                        Display.printSectionTitle("\nPassword: ");
+                        System.out.println(app.getSessionUser().getPsw());
+
+                        Display.printSectionTitle("\nNome: ");
+                        System.out.println(app.getSessionUser().getName());
+
+                        Display.printSectionTitle("\nCognome: ");
+                        System.out.println(app.getSessionUser().getSurname());
+
+                        Display.printSectionTitle("\nIndirizzo: ");
+                        System.out.println(app.getSessionUser().getAddress());
+
+                        Display.printSectionTitle("\nCodice fiscale: ");
+                        System.out.println(app.getSessionUser().getCf());
+
+
+                    } else {
+                        System.out.println();
+                        Display.printError("Per visualizzare le informazioni è necessario accedere con le proprie credenziali. ");
+                    }
+                    break;
                 case 5:
 
                     // controllo se l'utente ha eseguito il login
@@ -150,18 +188,121 @@ public class Main {
                         System.out.println();
                         Display.printError("Per creare una playlist è necessario accedere con le proprie credenziali. ");
 
-                        /*if (in.readYesNo("\nVuoi accedere all'area riservata? (yes/no) : ") == 'y'){
+                        if (in.readYesNo("\nVuoi accedere all'area riservata? (yes/no) : ") == 'y'){
                             String emailUser = in.readString( "Inserisci la tua email: ");
                             String pswUser = in.readString( "Inserisci password: ");
                             try {
                                 app.login(emailUser, pswUser);
                                 Display.printAuthSuccess("Accesso effettuato con successo!");
-
                             } catch (UserException e){
                                 Display.printAuthFailed(e.getMessage());
                             }
-                        }*/
+                        }
                     }
+                    break;
+                case 6:
+
+                    if(app.isLogged()) {
+
+                        Display.printSubtitle("\nINSERIMENTO EMOZIONI\n");
+
+                        Display.printSubtitle("Le tue playlist: ");
+                        Vector<Playlist> UserPlaylists = app.getPlaylistByUserId(app.getSessionUser().getUserId());
+                        System.out.println(UserPlaylists);
+
+                        Playlist playlist;
+                        do {
+                            String playlistName = in.readString("\nDigita il nome della playlist che vuoi selezionare: ");
+                            playlist = app.getPlaylistByName(playlistName);
+                            if (playlist == null) {
+                                Display.printError("Nessuna playlist corrispondente trovata! ");
+                            }
+                        } while (playlist == null);
+
+
+                        int songId = in.readInteger("\nDigita l'ID della canzone che vuoi selezionare: ");
+                        Song song = app.getSong(songId);
+
+                        Display.printListEmotions(app.getListEmotions());
+
+                        Display.printSectionTitle("\nInserisci per ogni emozione un voto da 1 (per niente) a 5 (molto) e aggiungi un'eventuale nota. ");
+
+                        char c;
+                        String nota1, nota2, nota3, nota4, nota5, nota6, nota7, nota8;
+                        int em1 = in.readInteger("\n* Emozione 1: ");
+                        c = in.readYesNo("Vuoi aggiungere una nota? (y/n) ");
+                        if (c == 'y') {
+                            nota1 = in.readString("Nota: ");
+                        }
+
+                        int em2 = in.readInteger("\n* Emozione 2: ");
+                        c = in.readYesNo("Vuoi aggiungere una nota? (y/n) ");
+                        if (c == 'y') {
+                            nota2 = in.readString("Nota: ");
+                        }
+
+                        int em3 = in.readInteger("\n* Emozione 3: ");
+                        c = in.readYesNo("Vuoi aggiungere una nota? (y/n) ");
+                        if (c == 'y') {
+                            nota3 = in.readString("Nota: ");
+                        }
+
+                        int em4 = in.readInteger("\n* Emozione 4: ");
+                        c = in.readYesNo("Vuoi aggiungere una nota? (y/n) ");
+                        if (c == 'y') {
+                            nota4 = in.readString("Nota: ");
+                        }
+
+                        int em5 = in.readInteger("\n* Emozione 5: ");
+                        c = in.readYesNo("Vuoi aggiungere una nota? (y/n) ");
+                        if (c == 'y') {
+                            nota5 = in.readString("Nota: ");
+                        }
+
+                        int em6 = in.readInteger("\n* Emozione 6: ");
+                        c = in.readYesNo("Vuoi aggiungere una nota? (y/n) ");
+                        if (c == 'y') {
+                            nota6 = in.readString("Nota: ");
+                        }
+
+                        int em7 = in.readInteger("\n* Emozione 7: ");
+                        c = in.readYesNo("Vuoi aggiungere una nota? (y/n) ");
+                        if (c == 'y') {
+                            nota7 = in.readString("Nota: ");
+                        }
+
+                        int em8 = in.readInteger("\n* Emozione 8: ");
+                        c = in.readYesNo("Vuoi aggiungere una nota? (y/n) ");
+                        if (c == 'y') {
+                            nota8 = in.readString("Nota: ");
+                        }
+
+
+
+
+                        // int emotionId = in.readInteger("\nDigita l'ID dell'emozione che vuoi selezionare: ");
+                        // Vector<Emotion> songEmotions = new Vector<>();
+                        // Emotion emotion = app.getEmotion(emotionId);
+                        // songEmotions.add(emotion);
+                        // playlist.addEmotion(song, emotion);
+                        // playlist.set(song, songEmotions);
+
+                    } else {
+                        System.out.println();
+                        Display.printError("Per creare una playlist è necessario accedere con le proprie credenziali. ");
+
+                        if (in.readYesNo("\nVuoi accedere all'area riservata? (yes/no) : ") == 'y'){
+                            String emailUser = in.readString( "Inserisci la tua email: ");
+                            String pswUser = in.readString( "Inserisci password: ");
+                            try {
+                                app.login(emailUser, pswUser);
+                                Display.printAuthSuccess("Accesso effettuato con successo!");
+                            } catch (UserException e){
+                                Display.printAuthFailed(e.getMessage());
+                            }
+                        }
+                    }
+
                     break;
 
                 case 7:
