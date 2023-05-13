22fa49b6bd4ea354ef576adef5b8d7eb01c428f1
Erik Gurzau <gurzau10@gmail.com>
2023-05-13 15:49:59 +0200

    fix nome colonna: codFiscale

--- script-sql/ES_CREATE_DB_TABLES.sql
+++ script-sql/ES_CREATE_DB_TABLES.sql
@@ -3,7 +3,7 @@ CREATE DATABASE Emotional_Songs;
 
 CREATE TABLE Utenti_Registrati (
     id SERIAL PRIMARY KEY,
-    cod_fiscale CHAR(16) NOT NULL,
+    codFiscale CHAR(16) NOT NULL,
     nome VARCHAR(50) NOT NULL,
     cognome VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL,
