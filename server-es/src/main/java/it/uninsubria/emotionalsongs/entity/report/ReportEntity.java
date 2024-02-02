c81a58c4365bfe1fa013f5c2d33a0a8cf14f971d
Sara Biavaschi <bia02.sara@libero.it>
2024-02-02 21:06:06 +0100

    feat: commenti per JavaDoc

--- server-es/src/main/java/it/uninsubria/emotionalsongs/entity/report/ReportEntity.java
+++ server-es/src/main/java/it/uninsubria/emotionalsongs/entity/report/ReportEntity.java
@@ -2,6 +2,13 @@ package it.uninsubria.emotionalsongs.entity.report;
 
 import java.util.List;
 
+/**
+ * Questa classe è responsabile della rappresentazione dell'entità relativa ad un report emozionale di un brano.
+ * @author Erik Gurzau (749400, VA)
+ * @author Alessia Metaj (738945, VA)
+ * @author Sara Biavaschi (748698, VA)
+ * @version 2.0.0
+ */
 public class ReportEntity {
 
     /**
@@ -10,33 +17,60 @@ public class ReportEntity {
     private Integer idCanzone;
 
     /**
-     * le informazioni del report in forma aggregata per uno stato emozionale.
+     * Le informazioni del report in forma aggregata per uno stato emozionale.
      */
     private List<EmotionalState> statiEmozionali;
 
+    /**
+     * Costruttore vuoto della classe.
+     */
     public ReportEntity() { }
 
+    /**
+     * Costruttore con parametri della classe.
+     * @param idCanzone L'id della canzone a cui si riferisce il report
+     * @param statiEmozionali Le informazioni del report in forma aggregata per uno stato emozionale
+     */
     public ReportEntity(Integer idCanzone, List<EmotionalState> statiEmozionali) {
         this.idCanzone = idCanzone;
         this.statiEmozionali = statiEmozionali;
     }
 
+    /**
+     * Getter dell'ID della canzone a cui si riferisce il metodo.
+     * @return L'intero che rappresenta l'ID della canzone
+     */
     public Integer getIdCanzone() {
         return idCanzone;
     }
 
+    /**
+     * Setter dell'ID della canzone a cui si riferisce il metodo.
+     * @param idCanzone L'intero che rappresenta l'ID della canzone
+     */
     public void setIdCanzone(Integer idCanzone) {
         this.idCanzone = idCanzone;
     }
 
+    /**
+     * Getter delle informazioni del report.
+     * @return La lista che rappresenta le informazioni
+     */
     public List<EmotionalState> getStatiEmozionali() {
         return statiEmozionali;
     }
 
+    /**
+     * Setter delle informazioni del report.
+     * @param statiEmozionali La lista che rappresenta le informazioni
+     */
     public void setStatiEmozionali(List<EmotionalState> statiEmozionali) {
         this.statiEmozionali = statiEmozionali;
     }
 
+    /**
+     * Sottoclasse rappresentante le informazioni del report per un singolo stato emozionale.
+     */
     public static class EmotionalState {
 
         /**
@@ -45,22 +79,32 @@ public class ReportEntity {
         private Integer idStatoEmozionale;
 
         /**
-         * Il numero di utenti che hanno recensito ciascuna emozione
+         * Il numero di utenti che hanno recensito l'emozione
          */
         private Integer numUtenti;
 
         /**
-         * La media del punteggio per ciascuna emozione.
+         * La media del punteggio per l'emozione.
          */
         private Double mediaPunteggio;
 
         /**
-         * Gli ultimi 5 commenti inseriti dagli utenti per ciascuna emozione
+         * Gli ultimi 5 commenti inseriti dagli utenti per l'emozione
          */
         private List<String> ultime5Note;
 
+        /**
+         * Costruttore vuoto della classe.
+         */
         public EmotionalState() { }
 
+        /**
+         * Costruttore con parametri della classe.
+         * @param idStatoEmozionale L'ID dello stato emozionale
+         * @param numUtenti Il numero di utenti che hanno recensito l'emozione
+         * @param mediaPunteggio La media del punteggio per l'emozione
+         * @param ultime5Note Gli ultimi 5 commenti per l'emozione
+         */
         public EmotionalState(Integer idStatoEmozionale, Integer numUtenti, Double mediaPunteggio, List<String> ultime5Note) {
             this.idStatoEmozionale = idStatoEmozionale;
             this.numUtenti = numUtenti;
@@ -68,36 +112,69 @@ public class ReportEntity {
             this.ultime5Note = ultime5Note;
         }
 
+        /**
+         * Getter dell'ID dello stato emozionale a cui si riferisce il report.
+         * @return L'intero che rappresenta l'ID dello stato emozionale
+         */
         public int getIdStatoEmozionale() {
             return idStatoEmozionale;
         }
 
+        /**
+         * Setter dell'ID dello stato emozionale a cui si riferisce il metodo.
+         * @param idStatoEmozionale L'intero che rappresenta l'ID dello stato emozionale
+         */
         public void setIdStatoEmozionale(Integer idStatoEmozionale) {
             this.idStatoEmozionale = idStatoEmozionale;
         }
 
+        /**
+         * Getter dell'ID del numero di utenti che hanno recensito l'emozione.
+         * @return L'intero che rappresenta il numero di utenti
+         */
         public Integer getNumUtenti() {
             return numUtenti;
         }
 
+        /**
+         * Setter del numero di utenti che hanno recensito l'emozione.
+         * @param numUtenti L'intero che rappresenta il numero di utenti
+         */
         public void setNumUtenti(Integer numUtenti) {
             this.numUtenti = numUtenti;
         }
 
+        /**
+         * Getter della media del punteggio per l'emozione.
+         * @return Double che rappresenta la media
+         */
         public double getMediaPunteggio() {
             return mediaPunteggio;
         }
 
+        /**
+         * Setter della media del punteggio per l'emozione.
+         * @param mediaPunteggio Double che rappresenta la media
+         */
         public void setMediaPunteggio(Double mediaPunteggio) {
             this.mediaPunteggio = mediaPunteggio;
         }
 
+        /**
+         * Getter degli ultimi 5 commenti inseriti dagli utenti per l'emozione.
+         * @return La lista di stringhe con i commenti
+         */
         public List<String> getUltime5Note() {
             return ultime5Note;
         }
 
+        /**
+         * Setter delgli ultimi 5 commenti inseriti dagli utenti per l'emozione.
+         * @param ultime5Note La lista di stringhe con i commenti
+         */
         public void setNote(List<String> ultime5Note) {
             this.ultime5Note = ultime5Note;
         }
     }
+
 }
--- server-es/src/main/java/it/uninsubria/emotionalsongs/entity/utente/UtenteRegistratoEntity.java
+++ server-es/src/main/java/it/uninsubria/emotionalsongs/entity/utente/UtenteRegistratoEntity.java
@@ -1,20 +1,82 @@
 package it.uninsubria.emotionalsongs.entity.utente;
 
+/**
+ * Questa classe è responsabile della rappresentazione dell'entità relativa ad un utente.
+ * @author Erik Gurzau (749400, VA)
+ * @author Alessia Metaj (738945, VA)
+ * @author Sara Biavaschi (748698, VA)
+ * @version 2.0.0
+ */
 public class UtenteRegistratoEntity {
 
+    /**
+     * L'ID dell'utente.
+     */
     private Integer id;
+
+    /**
+     * Il codice fiscale dell'utente.
+     */
     private String cod_fiscale;
+
+    /**
+     * Il nome dell'utente.
+     */
     private String nome;
+
+    /**
+     * Il cognome dell'utente.
+     */
     private String cognome;
+
+    /**
+     * L'email dell'utente.
+     */
     private String email;
+
+    /**
+     * La password dell'utente.
+     */
     private String psw;
+
+    /**
+     * L'indirizzo dell'utente.
+     */
     private String indirizzo;
+
+    /**
+     * Il CAP dell'utente.
+     */
     private String cap;
+
+    /**
+     * Il comune dell'utente.
+     */
     private String comune;
+
+    /**
+     * La provincia dell'utente.
+     */
     private String provincia;
 
+    /**
+     * Il costruttore vuoto della classe.
+     */
     public UtenteRegistratoEntity() { }
 
+    /**
+     * Il costruttore con parametri della classe.
+     * @param id L'ID dell'utente
+     * @param cod_fiscale Il codice fiscale dell'utente
+     * @param nome Il nome dell'utente
+     * @param cognome Il cognome dell'utente
+     * @param email L'email dell'utente
+     * @param psw La password dell'utente
+     * @param indirizzo L'indirizzo dell'utente
+     * @param cap Il CAP dell'utente
+     * @param comune Il comune dell'utente
+     * @param provincia La provincia dell'utente
+     */
     public UtenteRegistratoEntity(Integer id, String cod_fiscale, String nome, String cognome, String email, String psw, String indirizzo, String cap, String comune, String provincia) {
         this.id = id;
         this.cod_fiscale = cod_fiscale;
@@ -28,82 +90,162 @@ public class UtenteRegistratoEntity {
         this.provincia = provincia;
     }
 
+    /**
+     * Getter dell'ID dell'utente.
+     * @return L'intero che rappresenta l'ID dell'utente
+     */
     public Integer getId() {
         return id;
     }
 
+    /**
+     * Setter dell'ID dell'utente.
+     * @param id L'intero che rappresenta l'ID dell'utente
+     */
     public void setId(Integer id) {
         this.id = id;
     }
 
+    /**
+     * Getter del codice fiscale dell'utente.
+     * @return La stringa che rappresenta il codice fiscale
+     */
     public String getCodFiscale() {
         return cod_fiscale;
     }
 
+    /**
+     * Setter del codice fiscale dell'utente.
+     * @param cod_fiscale La stringa che rappresenta il codice fiscale
+     */
     public void setCodFiscale(String cod_fiscale) {
         this.cod_fiscale = cod_fiscale;
     }
 
+    /**
+     * Getter del nome dell'utente.
+     * @return La stringa che rappresenta il nome
+     */
     public String getNome() {
         return nome;
     }
 
+    /**
+     * Setter del nome dell'utente.
+     * @param nome La stringa che rappresenta il nome
+     */
     public void setNome(String nome) {
         this.nome = nome;
     }
 
+    /**
+     * Getter del cognome dell'utente.
+     * @return La stringa che rappresenta il cognome
+     */
     public String getCognome() {
         return cognome;
     }
 
+    /**
+     * Setter del cognome dell'utente.
+     * @param cognome La stringa che rappresenta il cognome
+     */
     public void setCognome(String cognome) {
         this.cognome = cognome;
     }
 
+    /**
+     * Getter dell'email dell'utente.
+     * @return La stringa che rappresenta l'email
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Setter dell'email dell'utente.
+     * @param email La stringa che rappresenta l'email
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * Getter della password dell'utente.
+     * @return La stringa che rappresenta la password
+     */
     public String getPsw() {
         return psw;
     }
 
+    /**
+     * Setter della password dell'utente.
+     * @param psw La stringa che rappresenta la password
+     */
     public void setPsw(String psw) {
         this.psw = psw;
     }
 
+    /**
+     * Getter dell'indirizzo dell'utente.
+     * @return La stringa che rappresenta l'indirizzo
+     */
     public String getIndirizzo() {
         return indirizzo;
     }
 
+    /**
+     * Setter dell'indirizzo dell'utente.
+     * @param indirizzo La stringa che rappresenta l'indirizzo
+     */
     public void setIndirizzo(String indirizzo) {
         this.indirizzo = indirizzo;
     }
 
+    /**
+     * Getter del CAP dell'utente.
+     * @return La stringa che rappresenta il CAP
+     */
     public String getCap() {
         return cap;
     }
 
+    /**
+     * Setter del CAP dell'utente.
+     * @param cap La stringa che rappresenta il CAP
+     */
     public void setCap(String cap) {
         this.cap = cap;
     }
 
+    /**
+     * Getter del comune dell'utente.
+     * @return La stringa che rappresenta il comune dell'utente
+     */
     public String getComune() {
         return comune;
     }
 
+    /**
+     * Setter del comune dell'utente.
+     * @param comune La stringa che rappresenta il comune
+     */
     public void setComune(String comune) {
         this.comune = comune;
     }
 
+    /**
+     * Getter della provincia dell'utente.
+     * @return La stringa che rappresenta la provincia
+     */
     public String getProvincia() {
         return provincia;
     }
 
+    /**
+     * Setter della provincia dell'utente.
+     * @param provincia La stringa che rappresenta la provincia
+     */
     public void setProvincia(String provincia) {
         this.provincia = provincia;
     }
